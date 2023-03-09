facea40e480e8dbfb808d34f8706abcb227659f2
Erool2006 <eryk.sanmedia@gmail.com>
2023-03-09 16:32:17 +0100

    Pusty program

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Czesc!! Wszystko dziala!");
+﻿var number1 = 15;
+var number2 = 10;
+
+// ==  != < > <= >=
+
+var name = "Eryk";
+var age = 57;
+
+// && znaczy się "i"
+// || znaczy się "lub" 
+// ! negacja - odwracanie (!(age < 50))
+
+if (age <= 57)
+{
+    Console.WriteLine("Jestem mlody");
+}
+else if (age >= 58)
+{
+    Console.WriteLine("Jestem sredni");
+}
+else if (age > 61)
+{
+    Console.WriteLine("Jestem stary dziad");
+}
